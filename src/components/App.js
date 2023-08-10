c742bebbcff88e6749ec0062e7e80fac96f70033
Stacy <obokostacy@gmail.com>
2023-08-11 01:09:18 +0300

    Added App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,26 @@
 import React from "react";
+import AlbumCard from './AlbumCard';
+import AlbumList from './AlbumList';
+import GenrePicker from './GenrePicker'; 
+import MainContent from './MainContent';
+import MenuLinks from './MenuLinks'; 
+import Search from './Search';
+import SideMenu from './SideMenu'; 
+import TopMenu from './TopMenu';
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return ( 
+   <div className="app-container">
+     <AlbumCard />
+     <AlbumList />
+     <GenrePicker />
+     <MainContent />
+     <MenuLinks />
+     <Search />
+     <SideMenu />
+     <TopMenu />
+  </div>
+  );
 }
 
 export default App;
